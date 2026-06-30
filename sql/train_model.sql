970019d40f5f263e7e2ca3c1675e4239d295f495
Muhammad Ehtisham Iqbal <ehtishamiqbal433@gmail.com>
2026-06-30 20:30:17 +0000

    fix: remove non-existent training_split column from BQML script

--- sql/train_model.sql
+++ sql/train_model.sql
@@ -1,11 +1,11 @@
--- Sample BigQuery ML Training Script
 CREATE OR REPLACE MODEL `forward-sentry-456317-k6.bioauth_dataset.bioauth_risk_model`
-OPTIONS(model_type='logistic_reg', input_label_cols=['is_anomaly']) AS
+OPTIONS(
+  model_type='logistic_reg', 
+  input_label_cols=['is_anomaly']
+) AS
 SELECT 
   hold_duration,
   flight_time,
   is_anomaly
 FROM 
-  `forward-sentry-456317-k6.bioauth_dataset.keystroke_behavioral_baselines`
-WHERE 
-  training_split = 'TRAIN';
+  `forward-sentry-456317-k6.bioauth_dataset.keystroke_behavioral_baselines`;
