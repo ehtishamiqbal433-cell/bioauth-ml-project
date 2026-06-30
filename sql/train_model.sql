a37b98421d5681e36d22bd90974e0a456e2e822e
Muhammad Ehtisham Iqbal <ehtishamiqbal433@gmail.com>
2026-06-30 20:56:25 +0000

    fix: align model training query features with active table schema

--- sql/train_model.sql
+++ sql/train_model.sql
@@ -1,11 +1,12 @@
 CREATE OR REPLACE MODEL `forward-sentry-456317-k6.bioauth_dataset.bioauth_risk_model`
 OPTIONS(
   model_type='logistic_reg', 
-  input_label_cols=['is_anomaly']
+  input_label_cols=['anomaly_label']
 ) AS
 SELECT 
-  hold_duration,
+  dwell_time,
   flight_time,
-  is_anomaly
+  speed_cps,
+  anomaly_label
 FROM 
   `forward-sentry-456317-k6.bioauth_dataset.keystroke_behavioral_baselines`;
