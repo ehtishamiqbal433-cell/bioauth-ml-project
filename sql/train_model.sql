127e02cd4162a4a56bc47720242e16c70386d238
Ehtisham <ehtishamiqbal433@gmail.com>
2026-07-24 20:47:29 +0300

    Update max_iterations in bioauth_risk_model
    Reduced max_iterations from 100 to 50 in model options.

--- sql/train_model.sql
+++ sql/train_model.sql
@@ -2,26 +2,18 @@ CREATE OR REPLACE MODEL `meta-coral-456317-g8.bioauth_ds.bioauth_risk_model`
 OPTIONS(
   model_type = 'BOOSTED_TREE_CLASSIFIER',
   input_label_cols = ['anomaly_label'],
-  max_iterations = 100,
+  max_iterations = 50,
   tree_method = 'HIST',
-  auto_class_weights = TRUE,
-  subsample = 0.85
+  auto_class_weights = TRUE
 ) AS
-SELECT
-  -- Core timing metrics
+SELECT 
   dwell_time,
   flight_time,
   speed_cps,
-
-  -- Advanced Kinematic & Feature Ratios
+  -- Advanced engineered features
   SAFE_DIVIDE(dwell_time, NULLIF(flight_time, 0)) AS dwell_to_flight_ratio,
   (dwell_time * speed_cps) AS cadence_intensity,
-
-  -- Micro-Behavioral & Variance Indicators
   ABS(dwell_time - flight_time) AS timing_asymmetry,
-  SAFE_DIVIDE(flight_time, NULLIF(speed_cps, 0)) AS flight_per_speed_unit,
-
-  -- Target label
   anomaly_label
-FROM
+FROM 
   `meta-coral-456317-g8.bioauth_ds.keystroke_behavioral_baselines`;
