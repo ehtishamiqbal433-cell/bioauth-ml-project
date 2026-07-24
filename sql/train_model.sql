fa49b53724c7a2c697ee8ae63fabd4090fd2b3e6
Ehtisham <ehtishamiqbal433@gmail.com>
2026-07-24 20:24:21 +0300

    Replace model creation with OR REPLACE syntax

--- sql/train_model.sql
+++ sql/train_model.sql
@@ -1,10 +1,6 @@
--- First, drop the model if it exists to avoid concurrent update issues
-DROP MODEL IF EXISTS `meta-coral-456317-g8.bioauth_ds.bioauth_risk_model`;
-
--- Then create a fresh model
-CREATE MODEL `meta-coral-456317-g8.bioauth_ds.bioauth_risk_model`
+CREATE OR REPLACE MODEL `meta-coral-456317-g8.bioauth_ds.bioauth_risk_model`
 OPTIONS(
-  model_type='logistic_reg', 
+  model_type='logistic_reg',
   input_label_cols=['anomaly_label']
 ) AS
 SELECT 
